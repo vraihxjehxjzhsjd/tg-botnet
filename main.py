74a170ceeb25574bfd9fc365cca77ecc6d7d86df
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-13 01:19:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from functions import storage
+
+with open("config.toml") as file:
+    config = toml.load(file)["sessions"]
+
+api_id = config["api_id"]
+api_hash = config["api_hash"]
+ 
+sessions = storage.sessions("sessions", api_id, api_hash)
+print("total accounts> %d \n" % len(sessions))
