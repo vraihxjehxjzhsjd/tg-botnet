d8e553784c0964589a6da81cc06441b2c40b4b9d
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-13 10:02:53 +0300

    Update register.py

--- functions/register.py
+++ functions/register.py
@@ -1,1 +1,7 @@
-pass
+from telethon.sync import TelegramClient
+import os, sys
+
+def session(path, api_id, api_hash):
+    file = path + "/" + str(len(os.listdir(path)))
+    TelegramClient(file, api_id, api_hash).start()
+    os.execl(sys.executable, sys.executable, *sys.argv) 
