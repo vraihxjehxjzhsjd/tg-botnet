0a4aed5fde8c0b826896653723563ac37e012d13
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-13 09:48:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ api_id = config["api_id"]
 api_hash = config["api_hash"]
 
 if storage.update is True:
-    register.main()
+    register.session()
 
 sessions = storage.sessions("sessions", api_id, api_hash)
 print("total accounts> %d \n" % len(sessions))
